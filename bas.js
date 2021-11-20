ac6197011ec5045d7c6a77bd4e527248075b0182
Cyberpunk007-error <77346617+Cyberpunk007-error@users.noreply.github.com>
2021-11-20 17:50:44 +0530

    V1

---
+++ bas.js
@@ -0,0 +1,100 @@
+//-----------------------------16/10/2021-------------------------------------------------------
+
+/*let fname=window.prompt("enter your name:")
+let pass=window.prompt("Enter your password:")
+/*let age =54
+let elligible = true
+let fav={
+    cn:"8/10",
+    pogo:"9/10"
+}
+console.log(fav.cn,fname,age,elligible)
+
+
+if(fname=="santhosh" && pass=="pass1234")
+{
+    console.log("welcome admin")
+}
+else if(fname=="san" && pass=="pass123"){
+    console.log("welcome student")
+}
+else {
+    console.log("welcome guest")
+}
+console.log("end")
+
+
+let fname=prompt("Enter your name: ")
+let age=Number(prompt("Enter your age"))
+console.log(typeof fname)
+console.log(typeof age)
+if(18 <= age && age <= 60){
+alert("elligible")
+
+alert("enter y or Y for yes and n or N for no")
+var N,n,Y,y
+let bp=prompt("tell us if u r having bp r not")
+let sugar=prompt("tell us if u r having sugar r not")
+    if( (bp=="N" || bp=="n") && (sugar=="N" || sugar=="n") ){
+        alert("u r elligible")
+}
+    else{
+        alert("not elligible")
+}
+}
+else{
+    alert("not elligible")
+}
+
+let no=Number(prompt("enter no"))
+switch(no){
+    case 1:
+        console.log("sun")
+    break    
+    case 2:
+        console.log("mon")
+        break
+        case 3:
+        console.log("sun")
+        break
+        default:
+        console.log("invalid")            
+}
+
+let regno=Number(prompt("enter the reg no"))
+let attper=Number(prompt("enter the att percentage"))
+var avt
+if(attper>=75){
+    alert("elligi")
+    let itm1=Number(prompt("enter the internal marks"))
+    let itm2=Number(prompt("enter the internal marks"))
+    avg=(itm1+itm2)/2
+    console.log(avg)
+    if(avg>=30){
+        alert("elligi to write exm")
+    }
+    else if(24<avg && avg<29){
+        alert("please enter yes r no")
+        let assign=prompt("submitted the assign r not")
+        if(assign=="yes"){alert("ellig")}
+        else{alert("not elli")}
+    }
+    else{
+        alert("not elligi to write ex")
+    }
+}
+else{alert("notelli")}
+
+//---------------------------------17/10/2021--------------------------------------------------
+for while
+
+let n=Number(prompt("Please enter"))
+for(i=n;i<=10;i++){
+    console.log("hi")
+}*/
+
+for(i=0;i<=10;i++){
+    if(i%2==0){
+        console.log(i)
+    }
+}
